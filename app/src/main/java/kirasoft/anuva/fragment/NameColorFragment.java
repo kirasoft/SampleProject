559e18893ed4175adff524ec76a479663338f903
Kota Justin <dekirasoft@gmail.com>
2016-12-14 23:06:06 -0500

    add name and color to list

--- app/src/main/java/kirasoft/anuva/fragment/NameColorFragment.java
+++ app/src/main/java/kirasoft/anuva/fragment/NameColorFragment.java
@@ -1,18 +1,27 @@
 package kirasoft.anuva.fragment;
 
+import android.app.Dialog;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import kirasoft.anuva.R;
 import kirasoft.anuva.adapter.NameColorAdapter;
 import kirasoft.anuva.model.NameBlock;
@@ -32,6 +41,9 @@ public class NameColorFragment extends Fragment {
     private int red = R.color.IndianRed;
     private int blue = R.color.Aquamarine;
     private int green = R.color.ForestGreen;
+
+    private int nbColor = red;
+    private String nbName = "";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,6 +66,71 @@ public class NameColorFragment extends Fragment {
         return rootView;
     }
 
+
+    @OnClick(R.id.button_add_name)
+    public void addNameClick() {
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.setContentView(R.layout.dialog_name_color);
+        dialog.setCancelable(true);
+        RadioGroup colorGroup = (RadioGroup) dialog.findViewById(R.id.dialog_radio_group);
+        EditText nameEdit = (EditText) dialog.findViewById(R.id.edit_name);
+        colorGroup.check(R.id.radio_red);
+        colorGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.radio_green:
+                        nbColor = green;
+                        break;
+                    case R.id.radio_blue:
+                        nbColor = blue;
+                        break;
+                    case R.id.radio_red:
+                        nbColor = red;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        nameEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                nbName = s.toString();
+            }
+        });
+
+        Button submit = (Button)dialog.findViewById(R.id.button_submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(nbName.isEmpty()){
+                    Toast.makeText(getActivity(), "Name can't be empty", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                NameBlock nameBlock = new NameBlock(nbName, nbColor);
+                nbName = "";
+                nbList.add(nameBlock);
+                colorAdapter.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+        });
+
+
+        dialog.show();
+
+    }
 
 
 }
