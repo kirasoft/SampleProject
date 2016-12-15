4fe6723f0836d3bce7de7c52bbcecfa31ada26c4
Kota Justin <dekirasoft@gmail.com>
2016-12-14 19:40:45 -0500

    Create Button list adapter with all drawables

--- app/src/main/java/kirasoft/anuva/adapter/ButtonListAdapter.java
+++ app/src/main/java/kirasoft/anuva/adapter/ButtonListAdapter.java
@@ -2,9 +2,18 @@ package kirasoft.anuva.adapter;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import kirasoft.anuva.R;
 
 /**
  * Created by dekir on 12/14/2016.
@@ -17,7 +26,38 @@ public class ButtonListAdapter extends ArrayAdapter<Drawable> {
         super(context, resource, drawables);
     }
 
+    //return single view of Button for list
+    @Override
+    public View getView(int position, View view, ViewGroup container){
 
+        ButtonListViewHolder viewHolder;
+        Drawable drawable = getItem(position);
+
+        if(view == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.button_list_item, null);
+            viewHolder = new ButtonListViewHolder(view);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ButtonListViewHolder)view.getTag();
+        }
+
+        if(viewHolder != null) {
+            viewHolder.image.setImageDrawable(drawable);
+        }
+
+        return view;
+    }
+
+    //button list view holder
+    //container for a reference to the imageview inside each item
+    private static class ButtonListViewHolder{
+        @BindView(R.id.button_list_image_view)
+        ImageView image;
+
+        public ButtonListViewHolder(View view){
+            ButterKnife.bind(this, view);
+        }
+    }
 
 
 
--- app/src/main/java/kirasoft/anuva/fragment/ListFragment.java
+++ app/src/main/java/kirasoft/anuva/fragment/ListFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,7 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import kirasoft.anuva.R;
+import kirasoft.anuva.adapter.ButtonListAdapter;
 
 /**
  * Created by Kota on 12/14/2016.
@@ -26,7 +28,7 @@ public class ListFragment extends Fragment {
     @BindView(R.id.list_view)
     ListView listView;
 
-    private ArrayAdapter<Drawable> adapter;
+
 
 
     @Override
@@ -41,8 +43,8 @@ public class ListFragment extends Fragment {
         //use butterknife to reduce boilerplate. (no view = findViewById..)
         ButterKnife.bind(this, getActivity());
 
-
-
+        ButtonListAdapter adapter = new ButtonListAdapter(getActivity(), 0, getButtonDrawablesList());
+        listView.setAdapter(adapter);
 
         return rootView;
     }
@@ -52,7 +54,8 @@ public class ListFragment extends Fragment {
     private List<Drawable> getButtonDrawablesList(){
         List<Drawable> drawableList = new ArrayList<>();
         for(int id : getResources().getIntArray(R.array.list_items)){
-            //
+            //add each drawable by id found in int-array in arrays.xml
+            drawableList.add(ContextCompat.getDrawable(getActivity(), id));
         }
         return drawableList;
     }
